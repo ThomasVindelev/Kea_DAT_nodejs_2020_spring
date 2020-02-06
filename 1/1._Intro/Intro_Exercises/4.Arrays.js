5766e1d77ee8f55eada998476dd765348e6bff55
ThomasVindelev <thom891b@stud.kea.dk>
2020-02-06 13:02:40 +0100

    loops

--- 1/1._Intro/Intro_Exercises/4.Arrays.js
+++ 1/1._Intro/Intro_Exercises/4.Arrays.js
@@ -15,7 +15,9 @@ var friends = [];
 
 // What a lonely array. Add at least 3 friend objects to it.  
 
-friends.push("Thomas", "Markus", "Gustav")
+friends.push(first = {"name": "Thomas"}, second = {"name": "Markus"}, third =  {"name": "Gustav"})
+
+console.table(friends);
 
 // --------------------------------------
 // Exercise 3 - Get the index of first occurance of that value. 
@@ -46,4 +48,49 @@ diet.splice(diet.indexOf("rocolla"), 0, "hamburger", "soda", "pizza");
 // You don't like kale at all. Remove the LAST element of the array.
 // Don't remove by index. You know in advance that it's the last in the array because you are too full already. 
 
-console.log(diet.pop)
+console.log(diet.pop);
+
+// --------------------------------------
+// Exercise 6 - Copy array
+
+var dinnerTray = [];
+// You really like your daily diet from last exercise. Copy it to a new array called dinnerTray so you can give it to a friend.  
+
+// --------------------------------------
+
+dinnerTray = diet.slice()  // start and end are optional
+
+console.table(dinnerTray);
+
+// --------------------------------------
+// Exercise 7 - For loop
+
+var letters = ["a","b","c", "d", "e", "f", "g", "h"];
+
+// log every second char in the array starting from b
+
+for (var i = 1; i < letters.length; i+= 2) {
+    console.log(letters[i]);
+}
+
+// --------------------------------------
+// Exercise 8 - For loop and if statement
+
+var numbers  = [5, 3, 2, 7, 11, 12, 0, -20, 6];
+
+var discardedNumbers = [];
+
+numbers.forEach(element => {
+    if (element > 6 || element < 0) {
+        console.log(element);
+    } else {
+        discardedNumbers.push(element);
+    }
+});
+
+console.table(discardedNumbers);
+
+// log the element if the number is above 6 or below 0
+// else push them to the array discardedNumbers
+
+// --------------------------------------
