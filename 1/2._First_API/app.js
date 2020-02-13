400db373c90bd08feaf8aabc597bbb420b4be123
ThomasVindelev <thom891b@stud.kea.dk>
2020-02-13 12:00:38 +0100

    Last commit for today

--- 1/2._First_API/app.js
+++ 1/2._First_API/app.js
@@ -8,7 +8,41 @@ app.get("/", (req, res) => {
 });
 
 app.get("/aboutMe", (req, res) => {
-    res.send({"firstName": "Thomas", "lastName": "Vindelev", "hobby": "Music"})
+    const me = {
+        "firstName": "Thomas", 
+        "lastName": "Vindelev", 
+        "hobby": "Music"
+    }
+    res.send(me)
 })
 
-app.listen(3000);
+app.listen(3000, error => {
+    if (error) {
+        console.log(error);
+    }
+    console.log("Server is running on port", 3000);
+});
+
+app.get("/aboutThisWebsite", (req, res) => {
+    const website = {
+        "port": "3000",
+        "type": "Rest API",
+        "purpose": "This is an explanation of the website"
+    };
+    res.send(website);
+    // any code after this won't run, because the send() function acts as a return statement
+});
+
+app.get("/countries", (req, res) => {
+    const denmark = {
+        "name": "Denmark"
+    };
+    const france = {
+        "name": "France"
+    };
+    const countries = {
+        denmark,
+        france
+    }
+    res.send(countries);
+});
