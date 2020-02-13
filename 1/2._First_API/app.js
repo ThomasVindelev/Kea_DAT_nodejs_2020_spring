5db90eb5bd82b1efec0a168c70e3845ed9d32abf
ThomasVindelev <thom891b@stud.kea.dk>
2020-02-13 10:43:28 +0100

    Hey

--- 1/2._First_API/app.js
+++ 1/2._First_API/app.js
@@ -3,4 +3,12 @@
 
 const app = require("express")();
 
+app.get("/", (req, res) => {
+    res.send({"message": "Hello there"});
+});
+
+app.get("/aboutMe", (req, res) => {
+    res.send({"firstName": "Thomas", "lastName": "Vindelev", "hobby": "Music"})
+})
+
 app.listen(3000);
